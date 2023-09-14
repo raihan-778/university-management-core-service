7776b93c588fead9e464f3ac0f193de1cb640d0e
raihan <raihanarif778@gmail.com>
2023-09-14 23:41:34 +0600

    first commit

---
+++ src/app/routes/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.routes';
+
+const router = express.Router();
+
+const moduleRoutes = [
+  // ... routes
+  {
+    path: '/academic-semesters',
+    routes: AcademicSemesterRoutes,
+  },
+];
+
+moduleRoutes.forEach(route => router.use(route.path, route.routes));
+export default router;
